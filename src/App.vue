bb76ba0fd0fb0c23773e14eebcf20da3e96f2b78
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-22 23:47:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+  export default {
+
+  }
+
+</script>
+
+<template>
+
+<h1>Productivity Site coming soon</h1>
+
+</template>
+
+<style lang="scss">
+
+@use './style/generic'
+
+
+</style>
